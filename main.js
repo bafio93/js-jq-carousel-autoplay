6f323f503d85f784e9678b7eedb9e9dfdb27af31
Previtali_Fabio <fabprev93@gmail.com>
2019-11-28 17:59:09 +0100

    Better coding

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         prev_img.addClass("active");
         current_dot.removeClass("active");
         prev_dot.addClass("active");
-    }
+    };
     $("i.fa-arrow-circle-left").click(scroll_left);
     // Arrow Right section:
     function scroll_right() {
@@ -29,19 +29,11 @@ $(document).ready(function(){
         next_img.addClass("active");
         current_dot.removeClass("active");
         next_dot.addClass("active");
-    }
+    };
     $("i.fa-arrow-circle-right").click(scroll_right);
-    // Automatic scroll every 3s.
+    //Bonus: selecting images from dots:
+    
+    // Automatic scroll every 3s:
     var clock = setInterval(scroll_right, 3000);
     scroll_right();
 });
-
-//  {
-//     var current_img = $("img.active");
-//     var next_img = current_img.next("img");
-//     if (next_img.length==0) {
-//         next_img = $("img:first-child");
-//     }
-//     current_img.removeClass("active");
-//     next_img.addClass("active");
-// }, 3000);
